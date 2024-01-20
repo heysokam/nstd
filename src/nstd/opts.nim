3d0c154de7c31ffa318d6d8bdb519f56cf7c4442
sOkam <7308253+heysokam@users.noreply.github.com>
2024-01-20 15:31:27 +0100

    new: Better cli.opts management. Supports opt values (was only bool)

--- src/nstd/opts.nim
+++ src/nstd/opts.nim
@@ -4,53 +4,74 @@
 # std dependencies
 import std/os
 import std/parseopt
+import std/tables
 
 
+#_________________________________________________
+# @section Types for CLI options
 #_____________________________
-# CLI options
-type ArgList * = seq[string]
-type OptList * = object
-  short *:seq[string]
-  long  *:seq[string]
+type ArgList    = seq[string]
+type OptEntries = OrderedTable[string,seq[string]]
+type OptList    = object
+  short *:OptEntries
+  long  *:OptEntries
 #_____________________________
 type CLI * = object
   args  *:ArgList
   opts  *:OptList
 
 #_________________________________________________
-# General CLI parse
+# @section Types Management aliases and forward exports
+#_____________________________
+func contains *(list :OptEntries; opt :string) :bool=  list.hasKey(opt)
+iterator items *(list :OptEntries) :string=
+  for key in list.keys: yield key
+
+#_________________________________________________
+# @section General CLI parse
 #_____________________________
 proc getCLI *() :CLI=
   var parser = commandLineParams().initOptParser()
   for kind, key, val in parser.getOpt():
     case kind
-    of cmdArgument:    result.args.add( key )
-    of cmdLongOption:  result.opts.long.add( key )
-    of cmdShortOption: result.opts.short.add( key )
-    of cmdEnd:         assert true
+    of cmdArgument                : result.args.add( key )
+    of cmdLongOption              :
+      if key in result.opts.long  : result.opts.long[key].add val
+      else                        : result.opts.long[key] = @[val]
+    of cmdShortOption             :
+      if key in result.opts.short : result.opts.short[key].add val
+      else                        : result.opts.short[key] = @[val]
+    of cmdEnd: assert true, "Reached a cmdEnd kind. Should never happen."
 
 #_________________________________________________
-# Get specific Elements
+# @section Get specific Elements
 #_____________________________
 proc getArgs *() :seq[string]=  getCLI().args
 proc getArg  *(id :SomeInteger) :string=  getCLI().args[id]
 proc getOpt  *(opt :char|string) :bool=
   ## Returns true if the given short option was passed in CLI
-  let cli   = getCLI()
-  let short = cli.opts.short()
-  let long  = cli.opts.long()
+  let cli = getCLI()
   # Treat the input as a short option
   when opt is char:
-    return $opt in short
+    return $opt in cli.opts.short
   elif opt is string:
     # Treat the input as a long option
     if opt.startsWith("--"):
-      return $opt[2..^1] in long
+      return $opt[2..^1] in cli.opts.long
     # Treat the input as a list of options in a single word, or as a short option starting with `-`
     else:
       var list :string= opt
       if list.startsWith("-"): list = list[1..^1]  # Remove the first character from the input
       for ch in list:
-        if ch notin short: return false  # Exit early when one of the characters of the `opt` input was not passed in cli
-      return true                        # All options of the `opt` input were passed in cli, so return true
+        if ch notin cli.opts.short: return false  # Exit early when one of the characters of the `opt` input was not passed in cli
+      return true                                 # All options of the `opt` input were passed in cli, so return true
+#___________________
+proc getLong *(cli :CLI; opt :string) :string=
+  for key,values in cli.opts.long.pairs:
+    if key == opt: return cli.opts.long[key][^1]
+#___________________
+iterator getLongIter *(cli :CLI; opt :string) :string=
+  for long,values in cli.opts.long.pairs:
+    if long != opt: continue
+    for entry in values: yield entry
 
