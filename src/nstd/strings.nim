1a280e321ac981f4f0f938f7b45ac3f3610b4711
sOkam <7308253+heysokam@users.noreply.github.com>
2024-03-18 04:08:03 +0100

    new: CstrArray automatic dealloc with `=destroy`

--- src/nstd/strings.nim
+++ src/nstd/strings.nim
@@ -1,23 +1,35 @@
 #:____________________________________________________
 #  nstd  |  Copyright (C) Ivan Mar (sOkam!)  |  MIT  :
 #:____________________________________________________
+# @deps nstd
+import ./types as nstd
 
 
-#_______________________________________________
-# strutils wrapper without empty string f*kery  |
-#_______________________________________________|
+#________________________________________________________
+# @section strutils wrapper without empty string f*kery  |
+#________________________________________________________|
+# @deps std
 import std/strutils as utils ; export utils except contains
 from   std/strutils as std import nil
 #_______________________________________
 proc contains *(A,B :string) :bool=
-  # The reason for this file to exist:
-  # assert A != "" and B != "", "Comparing two empty strings with `contains` is undecidable"
+  ## @reason for this section to exist:
+  ##  assert A != "" and B != "", "Comparing two empty strings with `contains` is undecidable"
+  ##  Strings in nim are considered sets, and the empty set triggers a Vacuous_truth in std.contains
+  ##  https://en.wikipedia.org/wiki/Vacuous_truth
+  ##  https://en.wikipedia.org/wiki/Syntactic_ambiguity
   if   B == "" and A != "" : return false  # "" in "something" == true   is just plain wrong
   elif A == "" and B == "" : return false  # undecidable. commit to false because nothing cannot contain anything
   elif B == A              : return true
   else: std.contains(A,B)
-  # note:
-  #   Strings in nim are considered sets, and the empty set triggers a Vacuous_truth in std.contains
-  #   https://en.wikipedia.org/wiki/Vacuous_truth
-  #   https://en.wikipedia.org/wiki/Syntactic_ambiguity
-#_______________________________________________|
+#_______________________________________________________|
+
+
+#_______________________________________
+# @section Cstring Array tools
+#_____________________________
+type CstrArray * = distinct cstringArray
+proc new *(_:typedesc[CstrArray]; list :openArray[string]) :CstrArray=
+  allocCstringArray(list).CstrArray
+proc `=destroy` *(arr :CstrArray) :void=
+  deallocCstringArray(arr.cstringArray)
