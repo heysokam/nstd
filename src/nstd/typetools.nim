d3e64a6a89fe9e43f5cb701256fcaada850e899b
sOkam <7308253+heysokam@users.noreply.github.com>
2024-03-27 14:42:21 +0100

    new: typetools.fieldList

--- src/nstd/typetools.nim
+++ src/nstd/typetools.nim
@@ -14,6 +14,17 @@ proc isType *(list :tuple; t :typedesc) :bool=
 template isObject *(T :typedesc) :bool=  T is object or T is ref object
   ## @descr Checks that the given type is an object.
 
+#___________________
+func fieldList *(T :typedesc) :seq[string]=
+  ## @descr Returns a sequence that contains the names of all fields in {@arg obj}
+  assert T.isObject, "nstd.typetools.fieldList(obj) can only be used on object types"
+  let tmp = default(T)
+  for field,val in tmp.fieldPairs:
+    result.add(
+      if field == "typ" : "type"
+      else              : field
+      ) # << result.add( ... )
+
 #__________________
 proc `new` *[T](x :T) :ref T=
   ## @descr
@@ -28,4 +39,6 @@ proc `new` *[T](x :T) :ref T=
 proc `@` *[T1, T2](val :T1; typ :typedesc[T2]) :T2=  cast[T2](val)
   ## @descr Casts the contents of {@arg val} to the given {@arg typ}
   ## @reason Syntax ergonomics. Same as MinC casting
+
+
 
