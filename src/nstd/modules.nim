0ee2051b5c7685248c860b22807ccc530ec86188
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-11 16:41:42 +0200

    fix: nstd.modules `import*` macros return an alphabetical order list

--- src/nstd/modules.nim
+++ src/nstd/modules.nim
@@ -4,6 +4,7 @@
 # @deps std
 from std/macros as m import add
 from std/os import lastPathPart
+from std/algorithm import reversed
 # @deps ndk
 from ./strings import endsWith
 
@@ -15,12 +16,17 @@ macro importAll *(dir :static[string]; importOnly :static[openArray[string]]= []
   # Generate the import list AST
   result = m.newStmtList()
   let filter = importOnly.len > 0
+  # └─ Create the list of imported files
+  var list :seq[string]
   for file in os.walkDir(dir):
     if not file.path.endsWith(".nim"): continue
     if filter and file.path.lastPathPart notin importOnly: continue
+    list.add file.path
+  # └─ Generate the import AST nodes
+  for file in list.reversed:
     result.add( m.newTree(
       m.nnkImportStmt,
-      m.newLit(file.path)
+      m.newLit(file)
       )) # << result.add( m.newTree( ... ) )
 #_____________________________
 macro importAllRec *(dir :static[string]; importOnly :static[openArray[string]]= []) :untyped=
@@ -30,9 +36,14 @@ macro importAllRec *(dir :static[string]; importOnly :static[openArray[string]]=
   # Generate the import list AST
   result = m.newStmtList()
   let filter = importOnly.len > 0
+  # └─ Create the list of imported files
+  var list :seq[string]
   for file in os.walkDirRec(dir):
     if not file.endsWith(".nim"): continue
     if filter and file.lastPathPart notin importOnly: continue
+    list.add file
+  # └─ Generate the import AST nodes
+  for file in list.reversed:
     result.add( m.newTree(
       m.nnkImportStmt,
       m.newLit(file)
