6f16bb6ddd657ef3965fec6273fce05199d965b1
sOkam <7308253+heysokam@users.noreply.github.com>
2024-06-20 03:24:11 +0200

    tst: Change existing tests into using `suite` and `expect`

--- tests/tGeneral.nim
+++ tests/tGeneral.nim
@@ -5,72 +5,76 @@ include ./base
 
 
 #____________________________________________________
-test nstd.Prefix&" Convert: Array to String":
-  check  ['a','b','c'].toString() == "abc"
-  check @['a','b','c'].toString() == "abc"
+suite nstd.Prefix&" Convert":
+  test "Array to String":
+    check  ['a','b','c'].toString() == "abc"
+    check @['a','b','c'].toString() == "abc"
 
 #____________________________________________________
-test nstd.Prefix&" Defines":
-  check not NimScript
+suite nstd.Prefix&" Defines":
+  test "nimscript":
+    check not defines.NimScript
+  test "debug":
+    check defines.debug == not (defined(release) or defined(danger)) or defined(debug)
 
 #____________________________________________________
-test nstd.Prefix&" Directories":
-  check projectDir() == ""
+suite nstd.Prefix&" Iterators":  # NOTE: should be separate file when there are more
+  test "twoD":
+    const bytesK = [
+      255'u8,255,255,255,
+         255,255,255,255,
+         255,255,255,255,
+         255,255,255,255,
+      ] # << bytesK[ ]
+    var bytes :array[16,u8]
+    for byte in bytes.twoD(4,4):
+      if byte == 0: byte = 255
+    for id,byte in bytes.pairs: check bytes[id] == bytesK[id]
 
 #____________________________________________________
-test nstd.Prefix&" Iterators":  # NOTE: should be separate file when there are more
-  const bytesK = [
-    255'u8,255,255,255,
-       255,255,255,255,
-       255,255,255,255,
-       255,255,255,255,
-    ] # << bytesK[ ]
-  var bytes :array[16,u8]
-  for byte in bytes.twoD(4,4):
-    if byte == 0: byte = 255
-  for id,byte in bytes.pairs: check bytes[id] == bytesK[id]
-
-#____________________________________________________
-test nstd.Prefix&" Type Tools":
-  let one,two :string= ""
-  let other :u8= 0
-  check (one,two).isType(string)
-  check not (one,other).isType(string)
+suite nstd.Prefix&" Type Tools":
+  test "isType":
+    let one,two :string= ""
+    let other :u8= 0
+    check (one,two).isType(string)
+    check not (one,other).isType(string)
   #________________________
-  type MyObj = object
-  type MyRef = ref object
-  check MyObj.isObject() and MyRef.isObject()
+  test "isObject":
+    type MyObj = object
+    type MyRef = ref object
+    check MyObj.isObject() and MyRef.isObject()
   #________________________
-  var val = MyObj()
-  check val.new() is ref MyObj
+  test "new T":
+    type MyObj = object
+    var val = MyObj()
+    check val.new() is ref MyObj
 
-#____________________________________________________
-proc  test *()= todo("todo"): "todo test"
-proc test2 *()= todo("two"): "todo test2"
-proc test3 *()= todo("three"): "todo test3"
-#____________________________________________________
-test nstd.Prefix&" Markers: Todo template":
-  todo("testing"): "todo test0"
-  todo "this is a todo test"
-  check report() == """
+suite nstd.Prefix&" Markers":
+  #_____________________________
+  proc  test ()= todo("todo"): "todo test"
+  proc test2 ()= todo("two"): "todo test2"
+  proc test3 ()= todo("three"): "todo test3"
+  #_____________________________
+  test nstd.Prefix&"Todo template":
+    todo("testing"): "todo test0"
+    todo "this is a todo test"
+    check report() == """
 two
-  tGeneral.nim(49,20): todo test2
+  tGeneral.nim(55,21): todo test2
 testing
-  tGeneral.nim(53,6): todo test0
+  tGeneral.nim(59,8): todo test0
 nstd
-  tGeneral.nim(54,2): this is a todo test
+  tGeneral.nim(60,4): this is a todo test
 three
-  tGeneral.nim(50,20): todo test3
+  tGeneral.nim(56,21): todo test3
 todo
-  tGeneral.nim(48,20): todo test
+  tGeneral.nim(54,21): todo test
 """
-
-#____________________________________________________
-test nstd.Prefix&" Markers: Unreachable":
-  proc marker=
-    if    false: discard
-    elif  false: discard
-    else: unreachable "----unreachable reason----"
-  try: marker(); check false
-  except Unreachable: check true
+  #_____________________________
+  test "Unreachable":
+    proc marker=
+      if    false: discard
+      elif  false: discard
+      else: unreachable "----unreachable reason----"
+    expect Unreachable: marker()
 
--- tests/tLogger.nim
+++ tests/tLogger.nim
@@ -9,89 +9,88 @@ import nstd/logger/helpers
 
 
 #____________________________________________________
-test nstd.Prefix&" Logger: Types":
-  check nstd.LogFunc is proc (args: varargs[string, `$`]) :void
-  check nstd.LvlFunc is proc (lvl :Log; args: varargs[string, `$`]) :void
-  check nstd.Log.high.ord == 8-1  # Panic if we added a level but didn't make a test for it
+suite nstd.Prefix&" Logger: Types":
+  test "Valid":
+    check nstd.LogFunc is proc (args: varargs[string, `$`]) :void
+    check nstd.LvlFunc is proc (lvl :Log; args: varargs[string, `$`]) :void
+    check nstd.Log.high.ord == 8-1  # Panic if we added a level but didn't make a test for it
 
 #____________________________________________________
-test nstd.Prefix&" Logger: Constructors":
-  let verb = when defined(release) or defined(danger): false elif defined(debug): true else: true
-  # Console Logger with Defaults
-  let con = newConLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_conLogger")
-  check con.name        == "tLogger_conLogger"
-  check con.kind        == logger.Kind.ConLogger
-  check con.threshold   == Log.All  # might fail if cfg.flushAll is changed for tests
-  check con.flushLvl    == Log.Fatal
-  check con.verbose     == verb
-  check con.file.path   == UndefinedPath
-  check con.file.mode   == fmAppend
-  check con.file.handle == stdout
-  # File Logger with Defaults
-  let file = newFileLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_fileLogger")
-  check file.name        == "tLogger_fileLogger"
-  check file.kind        == logger.Kind.FileLogger
-  check file.threshold   == Log.All  # might fail if cfg.flushAll is changed for tests
-  check file.flushLvl    == Log.Fatal
-  check file.verbose     == verb
-  check file.file.path   == helpers.getDefaultFile()
-  check file.file.mode   == fmAppend
-  check file.file.handle != nil
+suite nstd.Prefix&" Logger: Constructors":
+  let verb = when nstd.debug: true else: false
+  test "Logger with Defaults":
+    let con = newConLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_conLogger")
+    check con.name        == "tLogger_conLogger"
+    check con.kind        == logger.Kind.ConLogger
+    check con.threshold   == Log.All  # might fail if cfg.flushAll is changed for tests
+    check con.flushLvl    == Log.Fatal
+    check con.verbose     == verb
+    check con.file.path   == UndefinedPath
+    check con.file.mode   == fmAppend
+    check con.file.handle == stdout
+  test "Logger with Defaults":
+    let file = newFileLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_fileLogger")
+    check file.name        == "tLogger_fileLogger"
+    check file.kind        == logger.Kind.FileLogger
+    check file.threshold   == Log.All  # might fail if cfg.flushAll is changed for tests
+    check file.flushLvl    == Log.Fatal
+    check file.verbose     == verb
+    check file.file.path   == helpers.getDefaultFile()
+    check file.file.mode   == fmAppend
+    check file.file.handle != nil
 
 #____________________________________________________
-test nstd.Prefix&" Logger: Helpers":
-  # Log Level Keynames
-  check Log.Info.key  == "Info"
-  check Log.Wrn.key   == "Warn"
-  check Log.Err.key   == "Error"
-  check Log.Fatal.key == "Fatal"
-  check Log.Dbg.key   == "Debug"
-  check Log.Trc.key   == "Trace"
-  try: discard Log.None.key(); check false
-  except LogError: check true
-  try: discard Log.All.key(); check false
-  except LogError: check true
-  # Default File Path
-  check helpers.getDefaultFile().lastPathPart() == currentSourcePath().lastPathPart().changeFileExt("log").Path
-  # Log Format
-  check helpers.Sep == "|"
-  check PattDate    == "{getDateTime()}"
-  check PattLevel   == "{level.key()}"
-  check PattName    == "{logger.name}"
-  check PattMsg     == "{msg}"
-  check PattTrace   == "{getStackTrace()}"
-  when off:  # Cannot check these, because they have a date. #todo: Would need to regex or pattern match them
-    let con  = newConLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_conLogger")
-    echo con.formatMsg(Log.Info,  "test info message ",    42, 1234)
-    echo con.formatMsg(Log.Wrn,   "test warning message ", 42, 5678)
-    echo con.formatMsg(Log.Err,   "test error message ",   42, 9012)
-    echo con.formatMsg(Log.Fatal, "test fatal message ",   42, 3456)
-    echo con.formatMsg(Log.Dbg,   "test dbg message ",     42, 7890)
-    echo con.formatMsg(Log.Trc,   "test trc message ",     42, 1234)
+suite nstd.Prefix&" Logger: Helpers":
+  test "Log Level Keynames":
+    check Log.Info.key  == "Info"
+    check Log.Wrn.key   == "Warn"
+    check Log.Err.key   == "Error"
+    check Log.Fatal.key == "Fatal"
+    check Log.Dbg.key   == "Debug"
+    check Log.Trc.key   == "Trace"
+    expect LogError: discard Log.None.key()
+    expect LogError: discard Log.All.key()
+  test "Default File Path":
+    check helpers.getDefaultFile().lastPathPart() == currentSourcePath().lastPathPart().changeFileExt("log")
+  test "Log Format":
+    check helpers.Sep == "|"
+    check PattDate    == "{getDateTime()}"
+    check PattLevel   == "{level.key()}"
+    check PattName    == "{logger.name}"
+    check PattMsg     == "{msg}"
+    check PattTrace   == "{getStackTrace()}"
+    when off:  # Cannot check these, because they have a date. #todo: Would need to regex or pattern match them
+      let con  = newConLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_conLogger")
+      echo con.formatMsg(Log.Info,  "test info message ",    42, 1234)
+      echo con.formatMsg(Log.Wrn,   "test warning message ", 42, 5678)
+      echo con.formatMsg(Log.Err,   "test error message ",   42, 9012)
+      echo con.formatMsg(Log.Fatal, "test fatal message ",   42, 3456)
+      echo con.formatMsg(Log.Dbg,   "test dbg message ",     42, 7890)
+      echo con.formatMsg(Log.Trc,   "test trc message ",     42, 1234)
 #____________________________________________________
-test nstd.Prefix&" Logger: Helpers":
-  # File Logger process
-  let logg = newFileLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_fileLogger")
-  try: logg.log(Log.Info, "Test Writing to a file.")
-  except LogError: check false
-  check helpers.getDefaultFile().string.readFile != ""
-  logg.clear()
-  check helpers.getDefaultFile().string.readFile == ""
-  try:
-    for id in 0..<100: logg.log(Log.Info, "Test Writing to a file ", id)
-  except LogError: check false
-  try:
-    for id in 0..<25:
-      logg.log(Log.Info, "Test Writing to a file ", id)
-      logg.trc("Test Writing trace to a file ", id)
-      logg.dbg("Test Writing debug to a file ", id)
-      logg.info("Test Writing info  to a file ", id)
-      logg.wrn("Test Writing warn  to a file ", id)
-      logg.err("Test Writing error to a file ", id)
-      logg.fatal("Test Writing fatal to a file ", id)
-  except LogError: check false
-  check helpers.getDefaultFile().string.readFile != ""
-  logg.clear()
-  check helpers.getDefaultFile().string.readFile == ""
-  helpers.getDefaultFile().string.removeFile()
+suite nstd.Prefix&" Logger: Helpers":
+  test "File Logger process":
+    let logg = newFileLogger(currentSourcePath().lastPathPart().changeFileExt("")&"_fileLogger")
+    try                   : logg.log(Log.Info, "Test Writing to a file.")
+    except CatchableError : check false
+    check helpers.getDefaultFile().read != ""
+    logg.clear()
+    check helpers.getDefaultFile().read == ""
+    try:
+      for id in 0..<100: logg.log(Log.Info, "Test Writing to a file ", id)
+    except CatchableError : check false
+    try:
+      for id in 0..<25:
+        logg.log(Log.Info, "Test Writing to a file ", id)
+        logg.trc("Test Writing trace to a file ", id)
+        logg.dbg("Test Writing debug to a file ", id)
+        logg.info("Test Writing info  to a file ", id)
+        logg.wrn("Test Writing warn  to a file ", id)
+        logg.err("Test Writing error to a file ", id)
+        logg.fatal("Test Writing fatal to a file ", id)
+    except CatchableError : check false
+    check helpers.getDefaultFile().read != ""
+    logg.clear()
+    check helpers.getDefaultFile().read == ""
+    helpers.getDefaultFile().remove()
 
